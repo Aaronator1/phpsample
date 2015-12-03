4aae0802dcad4f95dea1112320b5361c18d32eaa
aaronmsmith <aaron.smith@healthways.com>
2015-12-03 06:59:10 -0600

    Added CA2

---
+++ CA2.php
@@ -0,0 +1,14 @@
+<html>
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: aaronmsmith
+ * Date: 12/3/15
+ * Time: 6:53 AM
+ */
+
+ echo "This is going to be a page with control statements."
+?>
+
+</html>
---
+++ hello.php
@@ -0,0 +1,9 @@
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+	 There should be php now. <BR/>
+ <?php echo '<p>Hello World</p>'; ?> 
+ </body>
+</html>
